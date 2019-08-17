b454925fe2cbf083c4167b8f8b272b747591d055
harish0796 <harish0796@gmail.com>
2019-08-17 10:03:31 +0530

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,68 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LikeComponent } from './like/like.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CourseComponent } from './course/course.component';
+import { ParentComponent } from './parent/parent.component';
+import { ChildComponent } from './child/child.component';
+import { OneComponent } from './one/one.component';
+import { TwoComponent } from './two/two.component';
+import { ThreeComponent } from './three/three.component';
+import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LikeComponent,
+    CoursesComponent,
+    CourseComponent,
+    ParentComponent,
+    ChildComponent,
+    OneComponent,
+    TwoComponent,
+    ThreeComponent,
+    NotFoundComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path:'',
+        component:CoursesComponent
+      },
+      {
+        path:'parent',
+        component:ParentComponent
+      },
+      {
+        path:'one',
+        component:OneComponent
+      },
+      {
+        path:'two',
+        component:TwoComponent
+      },
+      {
+        path:'three',
+        component:ThreeComponent
+      },
+      {
+        path:'register',
+        component:RegisterComponent
+      },
+      {
+        path:'**',
+        component:NotFoundComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
